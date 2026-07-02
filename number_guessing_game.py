9ddfefff4feaaceae195c01df7074db9f09136f2
Mocherla Kasthuri <mocherlakasthuri3@gmail.com>
2026-07-02 22:34:12 +0530

    Create number_guessing_game.py

---
+++ number_guessing_game.py
@@ -0,0 +1,23 @@
+"""
+Project: Number Guessing Game
+Author: Mocherla Kasthuri
+Description: A simple game where the user guesses a randomly generated number.
+"""
+
+import random
+
+secret_number = random.randint(1, 100)
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+
+while True:
+    guess = int(input("Enter your guess: "))
+
+    if guess < secret_number:
+        print("Too low! Try again.")
+    elif guess > secret_number:
+        print("Too high! Try again.")
+    else:
+        print("🎉 Congratulations! You guessed the correct number.")
+        break
