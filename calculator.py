f448e8046ee3cb2fb550ff737025ae247266ee63
Mocherla Kasthuri <mocherlakasthuri3@gmail.com>
2026-07-02 22:24:02 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,10 @@
+"""
+Project: Simple Calculator
+Author: Mocherla Kasthuri
+Description: A basic calculator that performs addition, subtraction,
+multiplication, and division based on user input.
+"""
+
 print("Simple Calculator")
 
 num1 = float(input("Enter first number: "))
